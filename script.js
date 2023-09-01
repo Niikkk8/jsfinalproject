8223e80f94820f0325d3897d688d98334e717afd
Niket Shah <shahniket643@gmail.com>
2023-09-01 14:57:39 +0530

    small changes before adding the filter by year option

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const searchInput = document.getElementById("movie_input");
 const movieListEl = document.querySelector('.movie_list');
+const movieTitleSearch = document.querySelector('.movie_header')
 
-async function renderMovies(filter) {
+async function renderMovies() {
     const movies = await fetch(`http://www.omdbapi.com/?s=${searchInput.value}&apikey=59430427`);
     const moviesData = await movies.json();
-    movieListEl.innerHTML = moviesData.Search.slice(0,6).map((movie) =>
-        `<div class="movie_card">
-            <div class="movie_card-container">
-                <img src="${movie.Poster}"
-                <div class="movie_description">
-                    <h3 class="movie_name"><b>${movie.Title}</b></h3>
-                    <p><b>${movie.Year}</b></p>
-                </div>
-            </div>
-        </div>`
-    ).join("");
-};
 
+    if (moviesData.Search) {
+        movieTitleSearch.innerHTML = `<h2 class="movie_header">Search results for <span class="movie_title-search">${searchInput.value}</span></h2>`
+        movieListEl.innerHTML = moviesData.Search.slice(0, 6).map((movie) =>
+            `<div class="movie_card">
+                    <img src="${movie.Poster}" alt="${movie.Title} Poster" class="movie_poster">
+                    <div class="movie_description">
+                        <h3 class="movie_name"><b>${movie.Title}</b></h3>
+                        <p class="movie_year"><b>${movie.Year}</b></p>
+                    </div>
+            </div>`
+        ).join("");
+    } else {
+        movieListEl.innerHTML = "<h2>No movies found.</h2>";
+    }
+}
