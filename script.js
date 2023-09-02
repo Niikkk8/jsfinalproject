46efbd822a1f1ceb3fc083496d8f4144cf7d801c
Niket Shah <shahniket643@gmail.com>
2023-09-02 10:33:45 +0530

    added responsiveness and progress bar

--- script.js
+++ script.js
@@ -6,48 +6,15 @@ const filterInput = document.getElementById("filter_input");
 const yearChange = document.getElementById("year");
 var filterStatus = false;
 
-// async function renderMovies() {
-//     const movies = await fetch(`http://www.omdbapi.com/?s=${searchInput.value}&apikey=59430427`);
-//     const moviesData = await movies.json();
-
-//     if (moviesData.Search) {
-//         movieTitleSearch.innerHTML = `<h2 class="movie_header">Search results for <span class="movie_title-search">"${searchInput.value}"</span></h2>`
-//         movieListEl.innerHTML = moviesData.Search.slice(0, 6).map((movie) =>
-//             `<div class="movie_card">
-//                     <img src="${movie.Poster}" alt="${movie.Title} Poster" class="movie_poster">
-//                     <div class="movie_description">
-//                         <h3 class="movie_name"><b>${movie.Title}</b></h3>
-//                         <p class="movie_year"><b>${movie.Year}</b></p>
-//                     </div>
-//             </div>`
-//         ).join("");
-//     } else {
-//         movieListEl.innerHTML = "<h2>No movies found.</h2>";
-//     }
-// }
-
-// function activeFilter(){
-//     filterStatus = !filterStatus;
-//     if(filterStatus == true){
-
-//     }
-//     else{
-        
-//     }
-// }
-
-// function filterChange() {
-//     yearChange.innerHTML = filterInput.value;
-// }
-l
-var allMovies = [];
+let allMovies = [];
 
 async function renderMovies() {
     const movies = await fetch(`http://www.omdbapi.com/?s=${searchInput.value}&apikey=59430427`);
     const moviesData = await movies.json();
+    document.body.classList += ' display_progress';
 
     if (moviesData.Search) {
-        movieTitleSearch.innerHTML = `<h2 class="movie_header">Search results for <span class="movie_title-search">"${searchInput.value}"</span></h2>`
+        movieTitleSearch.innerHTML = `<h2 class="movie_header">Search results for<br><span class="movie_title-search">"${searchInput.value}"</span></h2>`
         allMovies = moviesData.Search;
 
         const filteredMovies = filterMoviesByYear(allMovies, filterInput.value, filterStatus);
@@ -77,13 +44,18 @@ function filterMoviesByYear(movies, selectedYear, filterActive) {
 }
 
 function displayMovies(movies) {
-    movieListEl.innerHTML = movies.map((movie) =>
-        `<div class="movie_card">
+    setTimeout(() => {
+        movieListEl.innerHTML = movies.map((movie) =>
+            `<div class="movie_card">
+            <figure class="movie_poster-wrapper">
                 <img src="${movie.Poster}" alt="${movie.Title} Poster" class="movie_poster">
+            </figure>
                 <div class="movie_description">
                     <h3 class="movie_name"><b>${movie.Title}</b></h3>
                     <p class="movie_year"><b>${movie.Year}</b></p>
                 </div>
-        </div>`
-    ).join("");
+            </div>`
+        ).join("");
+        document.body.classList.remove('display_progress');
+    }, 1000);
 }
